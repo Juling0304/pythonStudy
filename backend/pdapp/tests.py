006acfabddb419629ae0be09dd45af1003006ba5
com928 <com928@naver.com>
2023-10-10 09:33:17 +0900

    fix: django.test 예제 추가

--- backend/pdapp/tests.py
+++ backend/pdapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+# python manage.py test 로 실행함.
+class TestCase(TestCase):
+
+    def test_add(self):
+        c = 20 + 10
+        self.assertEqual(c, 30)
+
+    def test_add_error(self):
+        c = 20 + 10
+        self.assertEqual(c, 40)
